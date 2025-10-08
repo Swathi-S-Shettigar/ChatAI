224f0e01114a3c922a5f284480a6da6960ad4a56
Swathi S Shettigar <148204048+Swathi-S-Shettigar@users.noreply.github.com>
2025-10-08 20:48:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ Return only SQL query using table 'uploaded_data'.
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
